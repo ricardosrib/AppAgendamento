5a3d2d3d218c85ad0ab3a007bb740d2f85df40f3
Ricardo Ribeiro <ricardoribeiro@Macbook-de-Ricardo.local>
2023-01-02 15:39:29 -0300

    Criação da quarta e quinta tela

--- app/src/main/java/com/example/appagendamento/HomeFragment.kt
+++ app/src/main/java/com/example/appagendamento/HomeFragment.kt
@@ -5,10 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val button1 = view.findViewById<ImageButton>(R.id.lightClean)
+        val button2 = view.findViewById<ImageButton>(R.id.mediumClean)
+        val button3 = view.findViewById<ImageButton>(R.id.heavyClean)
+
+
+        button1.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_setDateFragment)
+        }
+
+
+        button2.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_setDateFragment)
+        }
+
+
+        button3.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_setDateFragment)
+        }
 
 
     }
---
+++ app/src/main/java/com/example/appagendamento/SetDateFragment.kt
@@ -0,0 +1,15 @@
+package com.example.appagendamento
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class SetDateFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
