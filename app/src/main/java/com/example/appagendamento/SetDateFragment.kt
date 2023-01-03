774338023130bd09b7ae57cd6bc19f115444654c
RicardoSoaresDev <ricardosoares.job@gmail.com>
2023-01-02 22:58:31 -0300

    mudança de layout

--- app/src/main/java/com/example/appagendamento/SetDateFragment.kt
+++ app/src/main/java/com/example/appagendamento/SetDateFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
-class SetDateFragment : Fragment() {
+class SetDateFragment : Fragment(R.layout.fragment_set_date) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val setAppointment = view.findViewById<Button>(R.id.setAppointment)
+
+        setAppointment.setOnClickListener {
+            findNavController().navigate(R.id.action_setDateFragment_to_overviewFragment)
+        }
 
     }
 }
